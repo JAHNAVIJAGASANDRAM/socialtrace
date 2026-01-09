a1f2553fd99a8c202249d1fe6633a21f5fb51a42
jahnavijagasandram <jjahnavi2502@gmail.com>
2026-01-09 09:15:16 +0530

    added platform name

--- backend/app/engines/osint/username_reuse.py
+++ backend/app/engines/osint/username_reuse.py
@@ -1,5 +1,5 @@
 def check_username_reuse(username):
-    platforms = ["github.com", "reddit.com", "medium.com"]
+    platforms = ["github.com", "reddit.com", "medium.com","instagram.com","pinterest.com","x.com","linkedin.com"]
     found = []
 
     for p in platforms:
